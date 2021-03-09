19b2361d65ae988620dff2b166e5603f993c9b64
Rares Iordan <raresiordan09@gmail.com>
2021-03-09 13:38:16 +0200

    merged

---
+++ src/components/js/Home.js
@@ -0,0 +1,80 @@
+import React, {useEffect} from "react";
+import Rellax from "rellax";
+import HomeStart from "./HomeStart";
+import Content from "./Content";
+
+function Home() {
+    useEffect(() => {
+        new Rellax('.rellax');
+    });
+    return (
+        <React.Fragment>
+            <HomeStart/>
+            <Content type={'A'}>
+                <h3>Cops, another community I'm not part of.</h3>
+                <p>Tell him time is of the essence. Pretend. You pretend the feelings are there, for the world, for the
+                    people around you. Who knows? Maybe one day they will be. I'm going to tell you something that I've
+                    never told anyone before.</p>
+                <ul>
+                    <li>I'm real proud of you for coming, bro. I know you hate funerals.</li>
+                    <li>I'm Dexter, and I'm not sure what I am.</li>
+                    <li>I'm thinking two circus clowns dancing. You?</li>
+                </ul>
+            </Content>
+            <Content type={'B'}>
+                <p>You all right, Dexter? Watching ice melt. This is fun. Tell him time is of the essence. Only you
+                    could make those words cute. Keep your mind limber.</p>
+                <p>Tonight's the night. And it's going to happen again and again. It has to happen. Keep your mind
+                    limber. Finding a needle in a haystack isn't hard when every straw is computerized. I will not kill
+                    my sister. I will not kill my sister. I will not kill my sister.</p>
+                <p>I like seafood. Under normal circumstances, I'd take that as a compliment. I'm not the monster he
+                    wants me to be. So I'm neither man nor beast. I'm something new entirely. With my own set of rules.
+                    I'm Dexter. Boo.</p>
+                <p>I'm really more an apartment person. I like seafood. Pretend. You pretend the feelings are there, for
+                    the world, for the people around you. Who knows? Maybe one day they will be. I am not a killer.
+                    Makes me a … scientist.</p>
+            </Content>
+            <Content type={'C'}>
+                <p>God created pudding, and then he rested. Keep your mind limber. Makes me a … scientist. God created
+                    pudding, and then he rested. I'm doing mental jumping jacks.</p>
+                <p>Watching ice melt. This is fun. Oh I beg to differ, I think we have a lot to discuss. After all, you
+                    are a client. Pretend. You pretend the feelings are there, for the world, for the people around you.
+                    Who knows? Maybe one day they will be.</p>
+                <p>Pretend. You pretend the feelings are there, for the world, for the people around you. Who knows?
+                    Maybe one day they will be. Under normal circumstances, I'd take that as a compliment. You all
+                    right, Dexter?</p>
+                <p>I'm not the monster he wants me to be. So I'm neither man nor beast. I'm something new entirely. With
+                    my own set of rules. I'm Dexter. Boo. Somehow, I doubt that. You have a good heart, Dexter. You're a
+                    killer. I catch killers.</p>
+            </Content>
+            <Content type={'D'}>
+                <p>God created pudding, and then he rested. Keep your mind limber. Makes me a … scientist. God created
+                    pudding, and then he rested. I'm doing mental jumping jacks.</p>
+                <p>Watching ice melt. This is fun. Oh I beg to differ, I think we have a lot to discuss. After all, you
+                    are a client. Pretend. You pretend the feelings are there, for the world, for the people around you.
+                    Who knows? Maybe one day they will be.</p>
+                <p>Pretend. You pretend the feelings are there, for the world, for the people around you. Who knows?
+                    Maybe one day they will be. Under normal circumstances, I'd take that as a compliment. You all
+                    right, Dexter?</p>
+                <p>I'm not the monster he wants me to be. So I'm neither man nor beast. I'm something new entirely. With
+                    my own set of rules. I'm Dexter. Boo. Somehow, I doubt that. You have a good heart, Dexter. You're a
+                    killer. I catch killers.</p>
+            </Content>
+            <Content type={'E'}>
+                <p>God created pudding, and then he rested. Keep your mind limber. Makes me a … scientist. God created
+                    pudding, and then he rested. I'm doing mental jumping jacks.</p>
+                <p>Watching ice melt. This is fun. Oh I beg to differ, I think we have a lot to discuss. After all, you
+                    are a client. Pretend. You pretend the feelings are there, for the world, for the people around you.
+                    Who knows? Maybe one day they will be.</p>
+                <p>Pretend. You pretend the feelings are there, for the world, for the people around you. Who knows?
+                    Maybe one day they will be. Under normal circumstances, I'd take that as a compliment. You all
+                    right, Dexter?</p>
+                <p>I'm not the monster he wants me to be. So I'm neither man nor beast. I'm something new entirely. With
+                    my own set of rules. I'm Dexter. Boo. Somehow, I doubt that. You have a good heart, Dexter. You're a
+                    killer. I catch killers.</p>
+            </Content>
+        </React.Fragment>
+    );
+}
+
+export default Home;
---
+++ src/components/js/HomeStart.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ScrollAnimation from 'react-animate-on-scroll';
+import '../css/HomeStart.css';
+import {Link} from "react-router-dom";
+
+
+function HomeStart() {
+    return (
+        <div className={'homeStartContainer'}>
+            <div className={'homeStart'}>
+                <article className={'rellax'} data-rellax-zindex="2"
+                         style={{position: 'absolute', left: 0, right: 0, top: 0}}>
+                    <ScrollAnimation className={'leftSide'} animateIn={'animate__fadeIn'} animateOnce={true} delay={200}
+                                     duration={0.4}>
+                        <h1 className={'title'}>Brainer</h1>
+                        <h2 className={'startText'}>I'm something new entirely. With my own set of rules. I'm Dexter.
+                            Boo.</h2>
+                        <Link className={'btn startButton'} to={'/login'}>Sign Up</Link>
+                    </ScrollAnimation>
+                </article>
+                <div className={'rellax'} data-rellax-speed="-6" style={{
+                    position: 'absolute',
+                    right: 'calc(min(80vw, 80vh) * (-0.2))',
+                    bottom: 'calc(min(80vw, 80vh) * (-0.5))'
+                }}>
+                    <div className={'circle animate__animated animate__fadeInRightBig'}/>
+                </div>
+                <div className={'rellax'} data-rellax-zindex="3" data-rellax-speed="-4" style={{
+                    position: 'absolute',
+                    right: 'calc(min(25vw, 25vh) * 0.15)',
+                    bottom: 'calc(min(25vw, 25vh) * 0.8'
+                }}>
+                    <div className={'square animate__animated animate__fadeInUpBig'}/>
+                </div>
+                <div className={'rellax'} data-rellax-zindex="2" data-rellax-speed="-4" style={{
+                    position: 'absolute',
+                    right: 'calc(min(25vw, 25vh) * 1.3)',
+                    bottom: 'calc(min(25vw, 25vh) * 0.4'
+                }}>
+                    <div className={'square animate__animated animate__fadeInUpBig'}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomeStart;
