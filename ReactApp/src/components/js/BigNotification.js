9e44ae6e72a1057cea0120f31de1436ab414370b
Crimariu Codrin <codrincrismariu@gmail.com>
2021-04-20 19:10:56 +0300

    NotificationPage added

---
+++ ReactApp/src/components/js/BigNotification.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/BigNotification.css';
+
+function BigNotification(props) {
+    return (
+        <div className={'bigNotificationContainer'}>
+            <div className={'bigNotificationName'}>
+                {props.img == null ? props.name || props.value ? <img className={'bigNotificationImage'} src={"placeholder.jpg"} /> : '' : <img className={'bigNotificationImage'} src={props.img}/>}
+                {props.name}
+            </div>
+            <div className={"bigNotificationText"}>
+                {props.value}
+            </div>
+        </div>
+    )
+}
+
+export default BigNotification;
